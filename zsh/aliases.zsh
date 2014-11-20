4f80d14356c3c3397c474d37103a22145b72b3a2
Noah Crocker <necrocke@umich.edu>
2014-11-20 16:06:03 -0500

    Adding ZSH configs

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,25 @@
+# vi:syntax=zsh
+# -*- mode: sh -*-
+
+#
+# Shell aliases.
+#
+# Author:
+#   Noah Crocker <necrocke@umich.edu>
+#
+
 alias reload!='. ~/.zshrc'
+
+alias l="ls -alh"
+alias ll="ls -lh"
+alias lll="ls -ah"
+
+alias gosrc="cd ${SRC_PATH}"
+
+# more is always less
+if [[ -x $(which less) ]]; then
+  alias more="less"
+fi
+
+# Make sure stuff piped through less retains color
+alias less="less -R"
---
+++ zsh/functions.zsh
@@ -0,0 +1,28 @@
+# -*- mode: sh -*-
+
+#
+# Author:
+#   Noah Crocker <necrocke@umich.edu>
+#
+
+# Launch a static server in the current directory, passing an optional port
+# number
+http-serve() {
+  # Prefer http-server (`npm install -g http-server`) when available; fall back
+  # to a simple Python SimpleHTTPServer when unavailable. Note that
+  # SimpleHTTPServer has performance problems when serving more than a few files
+  # simultaneously (e.g. an HTML file with more than 2-3 linked files)
+  if [[ -x $(which http-server) ]]; then
+    $(which http-server)
+  else
+    $(which python) -m SimpleHTTPServer
+  fi
+}
+
+# Keep a process running, restarting it if it crashes
+always() {
+  until $1; do
+    echo "$1 died with exit code $?. Respawning..." >&2
+    sleep 1
+  done
+}
